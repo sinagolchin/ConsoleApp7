b54b1fecc901571d78d1a3d8fefc9d5ae1e8364e
sinagolchin <47105729+sinagolchin@users.noreply.github.com>
2019-02-20 14:42:19 +0330

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ConsoleApp7
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//        }
+//    }
+//}
+
+
+//namespace ConsoleApp7
+//{
+//    class Program
+//    {
+//        class Person
+//        {
+//            private readonly int age;
+//            public Person()
+//            {
+//                Console.WriteLine("Hi there");
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            Person p = new Person();
+//        }
+//    }
+//}
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
